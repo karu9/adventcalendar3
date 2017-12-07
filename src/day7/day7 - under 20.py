ff4e72b6b1a64eacc7de1e9b568bdc9c3ddce8aa
Charles PICHERY <charles.pichery@amadeus.com>
2017-12-07 17:37:11 +0100

    early teen solution

---
+++ src/day7/day7 - under 20.py
@@ -0,0 +1,13 @@
+## part 1 ##
+values = list(map (lambda x : [x[0:x.index(' ')], x[x.index('(')+1: x.index(')')], [] if '>' not in x else x[x.index('>') + 1 : -1].strip().split(', ')], open('input.txt').readlines()))
+## part 1 ##
+root = list(filter(lambda x : x[0] not in sum(list(map(lambda y : y[2], values)), []), values))[0]
+print(root)
+def findWeight(diskName):
+    disk = list(filter(lambda x : x[0] == diskName, values))[0]
+    weights = list(map(lambda x : int(findWeight(x)), disk[2]))
+    if len(set(weights)) == 2:
+        print(weights)
+        print(list(map(lambda x: list(filter(lambda y : y[0] == x, values))[0][1], disk[2])))
+    return sum(weights) + int(disk[1])
+findWeight(root[0])
