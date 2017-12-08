1cdc40694356720a048836b2bf7fe36c19db8495
Charles PICHERY <charles.pichery@amadeus.com>
2017-12-08 11:21:28 +0100

    day8 shorter

--- src/day8/day8 one liner.py
+++ src/day8/day8 one liner.py
@@ -1,6 +1,4 @@
-lines = open('input.txt').readlines()
 ## 0 = register 1 = action 2 = amount 4 = left condition 5 = comparison 6 = right condition
-operations = list(map(lambda x : x[:-1].split(' '), lines))
 registers = {}
 def operate(op):
     registers[op[4]] = registers[op[4]] if op[4] in registers.keys() else 0
@@ -8,6 +6,5 @@ def operate(op):
         registers[op[0]] = registers[op[0]] if op[0] in registers.keys() else 0
         exec("%s %s %s" % ("registers[op[0]] = registers[op[0]] ", '+' if op[1] == 'inc' else '-', int(op[2])))
     return max(registers.values())
-maxRegisters = list(map(lambda x : operate(x), operations))
-print(maxRegisters[-1])
-print(max(maxRegisters))
+maxRegisters = list(map(lambda x : operate(x), list(map(lambda x : x[:-1].split(' '), open('input.txt').readlines()))))
+print(maxRegisters[-1], max(maxRegisters))
