3cc82dd75da18f5ab8c8303123c0d4072ec5b4c1
Charles PICHERY <charles.pichery@amadeus.com>
2017-12-13 22:37:50 +0100

    day 11 corrected

--- src/day11/day11.py
+++ src/day11/day11.py
@@ -1,27 +1,5 @@
-## part 1 ##
-import math
-instructions = open('input.txt').readlines()[0].split(',')
-x = 0
-y = 0
-maximum = 0
-for instruction in instructions :
-    if instruction == 'n':
-        y += 1
-    elif instruction == 's':
-        y -= 1
-    elif instruction == 'nw':
-        x -= 1
-        y += 0.5
-    elif instruction == 'ne':
-        x += 1
-        y += 0.5
-    elif instruction == 'se':
-        x += 1
-        y -= 0.5
-    elif instruction == 'sw':
-        x -= 1
-        y -= 0.5
-    maximum = max(maximum, (max(abs(x), abs(y))))
-print(max(abs(x), abs(y)))
-print(maximum)
-    
+coords = [[0, 0]]
+for instruction in open('input.txt').readlines()[0].split(',') :
+    coords.append([x + y for x, y in zip(coords[-1], {'n':[0,1], 's':[0,-1], 'nw':[-0.5, 0.5], 'ne':[0.5,0.5], 'se':[0.5, -0.5], 'sw':[-0.5,-0.5]}[instruction])])
+print(int(2*abs(coords[-1][0]) if abs(coords[-1][0]) > abs(coords[-1][1]) else abs(coords[-1][0]) + abs(coords[-1][1])))
+print(max(list(map(lambda x : int(2*abs(x[0]) if abs(x[0]) > abs(x[1]) else abs(x[0]) + abs(x[1])), coords))))
