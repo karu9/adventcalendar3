f75a8e82a44db3bb27e8f50a6b0ec790b7c5023c
Charles PICHERY <charles.pichery@amadeus.com>
2017-12-13 14:45:25 +0100

    day13-2

--- src/day13/day13.py
+++ src/day13/day13.py
@@ -1,2 +1,6 @@
 firewalls = list(map(lambda x: [int(x[:x.index(':')]), int(x[x.index(':')+2: -1])], open('input.txt').readlines()))
 print(sum(list(map(lambda x : x[0] * x[1], list(filter(lambda x : x[0] % (2 * (x[1]-1)) == 0, firewalls))))))
+i = 1
+while len(list(filter(lambda x : (x[0] + i) % (2 * (x[1]-1)) == 0, firewalls))) != 0 :
+    i += 1
+print(i)
