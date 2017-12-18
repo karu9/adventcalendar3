db622831738ba7f640dbb161aa39f965f50ac2dc
Charles PICHERY <charles.pichery@amadeus.com>
2017-12-18 16:29:25 +0100

    refactor getintval

--- src/day18/day18-2.py
+++ src/day18/day18-2.py
@@ -1,16 +1,10 @@
 instructs = [x[:-1].split(' ') for x in open('input.txt').readlines()]
-def is_int(s):
-    try: 
-        int(s)
-        return True
-    except ValueError:
-        return False
 def initreg(reg, registers):
     if reg not in registers:
         registers[reg] = 0
 def getintval(val, registers):
     intval = 0
-    if not is_int(val):
+    if not val.replace('-', '').isdigit():
         intval = registers[val]
     else:
         intval = int(val)
@@ -52,13 +46,13 @@ def generate ():
     queue2 = []
     while vals1[1] or vals2[1]:
         if vals1[1]:
-            instruct1 = instructs[vals1[0]]
-            initreg(instruct1[1], registers1)
-            exec(instruct1[0] + '("' + instruct1[1] + '","' + instruct1[2] + '", registers1, vals1)' if len(instruct1) == 3 else instruct1[0] + '("' + instruct1[1] + '", registers1, vals1, vals2, queue1, queue2)')
+            instruct = instructs[vals1[0]]
+            initreg(instruct[1], registers1)
+            exec(instruct[0] + '("' + instruct[1] + '","' + instruct[2] + '", registers1, vals1)' if len(instruct) == 3 else instruct[0] + '("' + instruct[1] + '", registers1, vals1, vals2, queue1, queue2)')
         if vals2[1]:
-            instruct2 = instructs[vals2[0]]
-            initreg(instruct2[1], registers2)
-            exec(instruct2[0] + '("' + instruct2[1] + '","' + instruct2[2] + '", registers2, vals2)' if len(instruct2) == 3 else instruct2[0] + '("' + instruct2[1] + '", registers2, vals2, vals1, queue2, queue1)')
+            instruct = instructs[vals2[0]]
+            initreg(instruct[1], registers2)
+            exec(instruct[0] + '("' + instruct[1] + '","' + instruct[2] + '", registers2, vals2)' if len(instruct) == 3 else instruct[0] + '("' + instruct[1] + '", registers2, vals2, vals1, queue2, queue1)')
     print(vals1, vals2)
 generate()
 
