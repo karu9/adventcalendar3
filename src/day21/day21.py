2fb142ee0d97c1c58c5188de2a44ce90732e5700
Charles PICHERY <charles.pichery@amadeus.com>
2017-12-21 17:24:29 +0100

    d21'

--- src/day21/day21.py
+++ src/day21/day21.py
@@ -1,20 +1,23 @@
 rules = [[x[0: x.index(' =>')].split('/'), x[x.index('>')+ 2 : -1].split('/')] for x in open('input.txt').readlines()]
-my_grid = ['##..', '..#.', '#.#.', '....']
+my_grid = ['.#.', '..#', '###']
 def transform_grid(sub_grid):
+    matching_rules = []
     for rule in rules:
         rule_condition = sorted([sorted(x) for x in rule[0]])
         if len(rule_condition) == len(sub_grid):
             sorted_sub_grid = sorted([sorted(x) for x in sub_grid])
             if sorted_sub_grid == rule_condition:
-                return rule[1]
+                matching_rules.append(rule[1])
+    print(len(matching_rules))
+    return matching_rules[0]
 def regroup_grids(sub_grids):
-    print(sub_grids[1])
     grid = []
-    for x in range(len(sub_grids)):
-        sub_grid = []
-        for y in range(len(sub_grids)):
-            print(sub_grids[y][x]) 
-        grid.append(sub_grid)
+    for row in range(len(sub_grids)):
+        for subrow in range(len(sub_grids[0][0])):
+            val = ''
+            for column in range(len(sub_grids[0])):
+                val += sub_grids[row][column][subrow]
+            grid.append(val)
     return grid
 def get_transformed_grid(grid):
     slice_len = 2 if len(grid) % 2 == 0 else 3
@@ -23,8 +26,9 @@ def get_transformed_grid(grid):
     for x in range(number_of_grids):
         sub_grid = []
         for y in range(number_of_grids):
-            sub_grid.append(transform_grid([grid[z][x * slice_len: x * slice_len + slice_len] for z in range(y, y + slice_len)]))
+            sub_grid.append(transform_grid([grid[z][y * slice_len: y * slice_len + slice_len] for z in range(x * slice_len, x * slice_len + slice_len)]))
         sub_grids.append(sub_grid)
     return regroup_grids(sub_grids)
-print(get_transformed_grid(my_grid))
-                                   
+for i in range(5):
+    my_grid = get_transformed_grid(my_grid)
+    print(len(my_grid))
