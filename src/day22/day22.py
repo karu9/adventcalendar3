84e68cf15c43a9a14a727181f801805efa2ddb26
Charles PICHERY <charles.pichery@amadeus.com>
2017-12-22 11:31:01 +0100

    d22

--- src/day22/day22.py
+++ src/day22/day22.py
@@ -1,46 +1,49 @@
-grid = [x[:-1] for x in open('input.txt').readlines()]
-carrier = [int(len(grid[0])/2), int(len(grid)/2)]
-direction = [0, -1]
-counter = [0]
-def turn(is_left, direction):
-    if direction[0] == 0 and direction[1] == 1:
+GRID = [x[:-1] for x in open('input.txt').readlines()]
+CARRIER = [int(len(GRID[0])/2), int(len(GRID)/2)]
+DIR = [0, -1]
+counter = 0
+def turn(is_left):
+    if DIR[0] == 0 and DIR[1] == 1:
         if is_left:
-            direction = [1, 0]
+            DIR = [1, 0]
         else:
-            direction = [-1, 0]
-    elif direction[0] == 0 and direction[1] == -1:
+            DIR = [-1, 0]
+    elif DIR[0] == 0 and DIR[1] == -1:
         if not is_left:
-            direction = [1, 0]
+            DIR = [1, 0]
         else:
-            direction = [-1, 0]
-    elif direction[0] == 1 and direction[1] == 0:
+            DIR = [-1, 0]
+    elif DIR[0] == 1 and DIR[1] == 0:
         if is_left:
-            direction = [0, -1]
+            DIR = [0, -1]
         else:
-            direction = [0, 1]
+            DIR = [0, 1]
     else :
         if not is_left:
-            direction = [0, -1]
+            DIR = [0, -1]
         else:
-            direction = [0, 1]
-    carrier[0] += direction[0]
-    carrier[1] += direction[1]
-    if carrier[0] < 0 :
-        carrier[0] = 0
-        grid = ['.' + x for x in grid]
-    if carrier[0] == len(grid[0]):
-        grid = [x + '.' for x in grid]
-    return direction
-for i in range(10000):
-    if grid[carrier[1]][carrier[0]] == '#':
-        to_update = list(grid[carrier[1]])
-        to_update[carrier[0]] = '.'
-        grid[carrier[1]] = ''.join(to_update)
-        direction = turn(True, direction)
-    else :
-        to_update = list(grid[carrier[1]])
-        to_update[carrier[0]] = '#'
-        grid[carrier[1]] = ''.join(to_update)
-        counter[0] += 1
-        direction = turn(False, direction)
-print(counter[0])
+            DIR = [0, 1]
+    CARRIER[0] += DIR[0]
+    CARRIER[1] += DIR[1]
+    if CARRIER[0] < 0 :
+        CARRIER[0] = 0
+        GRID = ['.' + x for x in GRID]
+    if CARRIER[0] == len(GRID[0]):
+        GRID = [x + '.' for x in GRID]
+
+
+if __name__ == '__main__':
+    for i in range(10000):
+        print(len(GRID))
+        if GRID[CARRIER[1]][CARRIER[0]] == '#':
+            to_update = list(GRID[CARRIER[1]])
+            to_update[CARRIER[0]] = '.'
+            GRID[CARRIER[1]] = ''.join(to_update)
+            turn(True)
+        else :
+            to_update = list(GRID[CARRIER[1]])
+            to_update[CARRIER[0]] = '#'
+            GRID[CARRIER[1]] = ''.join(to_update)
+            counter += 1
+            turn(False)
+        print(counter)
