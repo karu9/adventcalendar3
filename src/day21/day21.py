8706fbcfb9cc8a1d95bc72628a6d0afa990bc916
Charles PICHERY <charles.pichery@amadeus.com>
2017-12-22 09:49:20 +0100

    day21 yay

--- src/day21/day21.py
+++ src/day21/day21.py
@@ -1,14 +1,27 @@
 rules = [[x[0: x.index(' =>')].split('/'), x[x.index('>')+ 2 : -1].split('/')] for x in open('input.txt').readlines()]
 my_grid = ['.#.', '..#', '###']
+def get_flips_and_rotations(rule):
+    result = []
+    result.append(rule)
+    ## rotations ##
+    len_rule = len(rule)
+    result.append([''.join([result[-1][x][k] for x in range(0,len_rule)]) for k in range(len_rule-1,-1,-1)])
+    result.append([''.join([result[-1][x][k] for x in range(0,len_rule)]) for k in range(len_rule-1,-1,-1)])
+    result.append([''.join([result[-1][x][k] for x in range(0,len_rule)]) for k in range(len_rule-1,-1,-1)])
+    ## flips ##
+    result.append(rule[::-1])
+    result.append([''.join([result[-1][x][k] for x in range(0,len_rule)]) for k in range(len_rule-1,-1,-1)])
+    result.append([''.join([result[-1][x][k] for x in range(0,len_rule)]) for k in range(len_rule-1,-1,-1)])
+    result.append([''.join([result[-1][x][k] for x in range(0,len_rule)]) for k in range(len_rule-1,-1,-1)])
+    return result
 def transform_grid(sub_grid):
     matching_rules = []
     for rule in rules:
-        rule_condition = sorted([sorted(x) for x in rule[0]])
-        if len(rule_condition) == len(sub_grid):
-            sorted_sub_grid = sorted([sorted(x) for x in sub_grid])
-            if sorted_sub_grid == rule_condition:
+        rule_root = rule[0]
+        if len(rule_root[0]) == len(sub_grid) and ''.join(rule_root).count('#') == ''.join(sub_grid).count('#'):
+            rule_flipped_rotated = ['/'.join(x) for x in get_flips_and_rotations(rule_root)]
+            if '/'.join(sub_grid) in rule_flipped_rotated:
                 matching_rules.append(rule[1])
-    print(len(matching_rules))
     return matching_rules[0]
 def regroup_grids(sub_grids):
     grid = []
@@ -29,6 +42,6 @@ def get_transformed_grid(grid):
             sub_grid.append(transform_grid([grid[z][y * slice_len: y * slice_len + slice_len] for z in range(x * slice_len, x * slice_len + slice_len)]))
         sub_grids.append(sub_grid)
     return regroup_grids(sub_grids)
-for i in range(5):
+for i in range(18):
     my_grid = get_transformed_grid(my_grid)
-    print(len(my_grid))
+    print(''.join(my_grid).count('#'))
